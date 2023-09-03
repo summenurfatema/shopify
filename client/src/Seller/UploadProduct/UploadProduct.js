98e8be515e81f20c717522c48fd33c4e9e44d226
jannatun-nur <shadrinmoni15@gmail.com>
2023-09-03 20:54:43 +0600

    product upload form added

--- client/src/Seller/UploadProduct/UploadProduct.js
+++ client/src/Seller/UploadProduct/UploadProduct.js
@@ -507,16 +507,4 @@ export default UploadProduct;
 //                   ))}
 //                 </select>
 //               </div>
-//               {/* available */}
-//               <div className="col-span-full sm:col-span-2">
-//                 <label for="availavle">Available</label>
 
-//                 <select
-
-//                   id="availavle"
-//                   className="w-full rounded-md border h-12 focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900"
-//                 >
-//                   <option value="true">Yes</option>
-//                   <option value="false">No</option>
-//                 </select>
-//               </div>
