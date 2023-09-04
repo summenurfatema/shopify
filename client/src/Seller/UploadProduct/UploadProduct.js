a82827a713a3445ec126551048d268d123180337
jannatun-nur <shadrinmoni15@gmail.com>
2023-09-04 21:33:30 +0600

    post method added

--- client/src/Seller/UploadProduct/UploadProduct.js
+++ client/src/Seller/UploadProduct/UploadProduct.js
@@ -432,18 +432,7 @@ export default UploadProduct;
 //                   type="text"
 
 //                   className="w-full rounded-md border h-12 focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900"
-//                 />
-//               </div>
 
-
-//                   className="w-full rounded-md border h-12 focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900"
-//                 />
-//               </div>
-//               {/* color */}
-//               <div className="col-span-full sm:col-span-2">
-//                 <label for="color">Color</label>
-
-//                 <select
 
 //                   id="color"
 //                   className="w-full rounded-md border h-12 focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900"
