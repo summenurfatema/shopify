cc7799513171a2c8b82377f47a76d67bf8cdb761
jannatun-nur <shadrinmoni15@gmail.com>
2023-09-07 11:39:11 +0600

    featured product added

--- client/src/HomePage/HomePage.js
+++ client/src/HomePage/HomePage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AllProduct from '../pages/ProductSection/AllProduct/AllProduct';
 import HeroSlider from '../pages/HeroSlider/HeroSlider';
+import FeaturedProduct from '../pages/FeaturedSection/FeaturedProduct';
 
 const HomePage = () => {
     return (
         <div>
             <HeroSlider/>
             <AllProduct/>
+            <FeaturedProduct/>
         </div>
     );
 };
--- client/src/pages/ProductSection/AllProduct/AllProduct.js
+++ client/src/pages/ProductSection/AllProduct/AllProduct.js
@@ -14,7 +14,7 @@ const AllProduct = () => {
 ;
   console.log(products);
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 px-0 md:px-10 2xl:px-14 3xl:px-20 py-10 gap-5 lg:gap-6 font-sans">
+    <div className="bg-white grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 px-0 md:px-10 2xl:px-14 3xl:px-20 py-10 gap-5 lg:gap-6 font-sans">
       {products.map((product) => (
         <Link
           to={`/data/${product._id}`}
