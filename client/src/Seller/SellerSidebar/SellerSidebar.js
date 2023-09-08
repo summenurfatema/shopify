392abbe929f2907d637ac1f689d30dbd2fdbc748
jannatun-nur <shadrinmoni15@gmail.com>
2023-09-08 23:07:36 +0600

    completed

--- client/src/Seller/SellerSidebar/SellerSidebar.js
+++ client/src/Seller/SellerSidebar/SellerSidebar.js
@@ -4,7 +4,7 @@ import {ImUpload} from 'react-icons/im'
 import {MdProductionQuantityLimits} from 'react-icons/md'
 import {RiDoorOpenFill} from 'react-icons/ri'
 import {FaBox} from 'react-icons/fa'
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const SellerSidebar = () => {
 
@@ -32,32 +32,32 @@ const SellerSidebar = () => {
         <div className="divide-y divide-gray-700">
             <ul className="pt-2 pb-4 space-y-1 text-sm">
                 <li className="bg-white text-gray-800 hover:bg-indigo-600 hover:text-white rounded-md">
-                    <Link rel="noopener noreferrer" href="/seller" className="flex items-center p-2 space-x-3 rounded-md">
+                    <a rel="noopener noreferrer" href="/seller" className="flex items-center p-2 space-x-3 rounded-md">
                     <MdProductionQuantityLimits className='text-xl' />
                         <span className='font-semibold text-lg'>My Products</span>
-                    </Link>
+                    </a>
                 </li>
                 <li className='bg-white text-gray-800 hover:bg-indigo-600 hover:text-white rounded-md'>
-                    <Link rel="noopener noreferrer" href="/seller/upload-product" className="flex items-center p-2 space-x-3 rounded-md">
+                    <a rel="noopener noreferrer" href="/seller/upload-product" className="flex items-center p-2 space-x-3 rounded-md">
                     <ImUpload className='text-xl' />
                         <span className='font-semibold text-lg'>Upload a product</span>
-                    </Link>
+                    </a>
                 </li>
                 <li className='bg-white text-gray-800 hover:bg-indigo-600 hover:text-white rounded-md'>
-                    <Link rel="noopener noreferrer" href='/seller/current-order' className="flex items-center p-2 space-x-3 rounded-md">
+                    <a rel="noopener noreferrer" href='/seller/current-order' className="flex items-center p-2 space-x-3 rounded-md">
                     <FaBox className='text-xl' />
                         <span className='font-semibold text-lg'>Current orders</span>
-                    </Link>
+                    </a>
                 </li>
                 
             </ul>
             <ul className="pt-4 pb-2 space-y-1 text-sm">
               
                 <li onClick={handleLogOut} className='bg-white text-gray-800 hover:bg-indigo-600 hover:text-white rounded-md'>
-                    <Link rel="noopener noreferrer" href="/" className="flex items-center p-2 space-x-3 rounded-md">
+                    <a rel="noopener noreferrer" href="/" className="flex items-center p-2 space-x-3 rounded-md">
                     <RiDoorOpenFill className='text-xl' />
                         <span className='font-semibold text-lg'>Logout</span>
-                    </Link>
+                    </a>
                 </li>
             </ul>
         </div>
