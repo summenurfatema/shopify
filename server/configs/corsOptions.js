8ae379f06068e804d3494f443fbb30fca460ef1f
devlopersabbir <devlopersabbir@gmail.com>
2023-09-21 16:39:32 +0600

    cors options configs

--- server/configs/corsOptions.js
+++ server/configs/corsOptions.js
@@ -1,8 +1,21 @@
 import * as dotenv from "dotenv";
+import { allowedOrigins } from "./allowedOrigin";
 dotenv.config();
 
 const corsOption = {
-  origin: (origin, callBack) => {
-    if()
+  origin: (requestOrigin, callBack) => {
+    if (!requestOrigin) {
+      callBack(null, true);
+    } else {
+      if (allowedOrigins.indexOf(requestOrigin) !== -1) {
+        callBack(null, true);
+      } else {
+        callBack(new Error("Not allowed by CORS!"));
+      }
+    }
   },
+  credential: true,
+  optionsSuccessStatus: 200,
 };
+
+export default corsOption;
